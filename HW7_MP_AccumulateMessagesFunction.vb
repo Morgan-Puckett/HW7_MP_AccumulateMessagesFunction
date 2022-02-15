8dc89bd0174536545d2f5e5b6bce5bdb405ba250
Morgan Puckett <morganpuckett02@gmail.com>
2022-02-14 18:54:20 -0700

    Add project files.

---
+++ HW7_MP_AccumulateMessagesFunction.vb
@@ -0,0 +1,41 @@
+﻿'Morgan Puckett
+'Rcet0265
+'Spring 2022
+'Accumlulate messages
+'
+
+
+
+Option Explicit On
+Option Strict On
+Option Compare Text
+
+Module HW7_MP_AccumulateMessagesFunction
+    Sub Main()
+        Dim userInput As String
+        Do
+            Do
+                'this displays the fuctionality of UserMessages Function
+                Console.WriteLine("If you press ""Q"" the program will end")
+                userInput = Console.ReadLine()
+                UserMessages(userInput, False)
+                Console.WriteLine(UserMessages("", False))
+            Loop While userInput <> "Q"
+            UserMessages(CStr(vbEmpty), True)
+        Loop Until userInput = "Q"
+        Console.ReadLine()
+    End Sub
+
+
+    'this function will accumulate the messages by adding the latest message on a new line
+    Function UserMessages(ByVal newMessage As String, ByVal clear As Boolean) As String
+        Static messages As String
+        If clear = True Then
+            messages = ""
+        ElseIf newMessage <> "" Then
+            messages &= newMessage & vbNewLine
+
+        End If
+        Return messages
+    End Function
+End Module
